4f1978f8d888d65ec8625db17dc9b370133a51ed
garthdb <garthdb@gmail.com>
2016-06-27 17:14:36 -0600

    tests page load speed and scroll performance

---
+++ src/frames-util.js
@@ -0,0 +1,57 @@
+export default class FramesUtil {
+  constructor(frames){
+    this.frames = frames;
+    this.framesAnalysis = this._analyzeFrames(this.frames);
+  }
+  _analyzeFrames(frames){
+    let result = {
+      repaints: 0,
+      largeFrames: [],
+      totalDuration: 0,
+      timeByCategory: {idle: 0},
+    };
+    frames.forEach((frame, index) => {
+      if(frame.duration > 18){
+        result.largeFrames.push(frame);
+      }
+      result.totalDuration += frame.duration;
+      result.repaints += frame.paints.length;
+
+      let categoryTotal = 0;
+
+      for (let category in frame.timeByCategory) {
+        if(!result.timeByCategory.hasOwnProperty(category)){
+          result.timeByCategory[category] = frame.timeByCategory[category];
+        } else {
+          result.timeByCategory[category] += frame.timeByCategory[category];
+        }
+        categoryTotal += frame.timeByCategory[category];
+      }
+      result.timeByCategory.idle += frame.duration - categoryTotal;
+    });
+
+    return result;
+  }
+  _roundDigits(integer, count = 2){
+    let power = Math.pow(10,count);
+    return Math.round(integer * power)/power;
+  }
+  getAverageFrameRate() {
+    return this._roundDigits(1/((this.framesAnalysis.totalDuration / this.frames.length) / 1000));
+  }
+  getTotalFrameCount() {
+    return this.frames.length;
+  }
+  getTotalLargeFrameCount () {
+    return this.framesAnalysis.largeFrames.length;
+  }
+  getBreakDownPercentage () {
+    let result = {};
+    for (let category in this.framesAnalysis.timeByCategory) {
+      let categoryTime = this.framesAnalysis.timeByCategory[category];
+      let totalTime = this.framesAnalysis.totalDuration;
+      result[category] = this._roundDigits((categoryTime/totalTime) * 100) + "%";
+    }
+    return result;
+  }
+}
--- src/toplemetry.js
+++ src/toplemetry.js
@@ -1,10 +1,13 @@
 import Chrome from 'chrome-remote-interface';
+import DevtoolsTimelineModel from 'devtools-timeline-model';
+import FramesUtil from './frames-util';
+
+let TRACE_CATEGORIES = ["-*", "devtools.timeline", "disabled-by-default-devtools.timeline", "disabled-by-default-devtools.timeline.frame", "toplevel", "blink.console", "disabled-by-default-devtools.timeline.stack", "disabled-by-default-devtools.screenshot", "disabled-by-default-v8.cpu_profile"];
 
 export default class Toplemetry {
   constructor(port, url){
     this.port = (typeof port !== 'undefined')?  port : 9222;
     this.url = (typeof url !== 'undefined')?  url : 'http://topcoat.io';
-    let TRACE_CATEGORIES = ["-*", "devtools.timeline", "disabled-by-default-devtools.timeline", "disabled-by-default-devtools.timeline.frame", "toplevel", "blink.console", "disabled-by-default-devtools.timeline.stack", "disabled-by-default-devtools.screenshot", "disabled-by-default-v8.cpu_profile"];
   }
   chromeSetup(callback){
     if (typeof this.chrome == 'undefined'){
@@ -28,7 +31,6 @@ export default class Toplemetry {
     });
   }
   runAll(callback){
-    console.log('start async');
     this.loading(()=> {
       this.scrolling(()=>{
         Chrome.Close({id: this.tab.id},()=>{
@@ -51,7 +53,14 @@ export default class Toplemetry {
       this.chrome.Page.loadEventFired( params => {
         requestEndTime = params.timestamp;
         this.loadtime = parseFloat(requestEndTime) - parseFloat(requestStartTime);
-        callback();
+        this.chrome.Runtime.evaluate({expression: "document.body.clientHeight - window.innerHeight"}, (err, params)=>{
+          if(!err && params.result.value > 0){
+            this.scrollHeight = -Math.abs(parseInt(params.result.value));
+          } else {
+            throw new Error('Page is too small to scroll');
+          }
+          callback();
+        });
       });
       this.chrome.once('ready', () => {
         this.chrome.Page.navigate({url: this.url});
@@ -60,11 +69,34 @@ export default class Toplemetry {
   }
   scrolling(callback){
     this.chromeSetup(()=>{
-      console.log('hello');
+
+      let rawEvents = [];
+
+      this.chrome.Tracing.dataCollected(function(data){
+        var events = data.value;
+        rawEvents = rawEvents.concat(events);
+
+      });
+
       this.chrome.Tracing.start({
         "categories": TRACE_CATEGORIES.join(','),
         "options": "sampling-frequency=10000"
       });
+      this.chrome.Input.synthesizeScrollGesture({x: 0, y: 0, xDistance: 0, yDistance: this.scrollHeight, gestureSourceType: 'mouse', speed: 1600},(err,params)=>{
+        this.chrome.Tracing.end();
+      });
+
+      this.chrome.Tracing.tracingComplete(function () {
+        let model = new DevtoolsTimelineModel(rawEvents);
+        let frames = new FramesUtil(model.frameModel()._frames);
+        console.log(`Total Frame Count ${frames.getTotalFrameCount()}`);
+        console.log(`Average Frame Rate ${frames.getAverageFrameRate()} fps`);
+        console.log(`Total Large Frame Count ${frames.getTotalLargeFrameCount()}`);
+        console.log('Frame time breakdown');
+        console.log(frames.getBreakDownPercentage());
+        callback();
+      });
+
     });
   }
 }
