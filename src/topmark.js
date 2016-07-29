11750ff38d705ab385192baf8e4bbf87b4221eda
garthdb <garthdb@gmail.com>
2016-07-29 03:10:57 -0600

    added eslint

--- src/topmark.js
+++ src/topmark.js
@@ -4,31 +4,31 @@ import remiAddResults from 'remi-topmarks-results';
 
 export default class Topmark {
   constructor(options = {}) {
-    this._registrator = remi(this);
-    this._registrator.hook(remiRunner());
-    this._registrator.hook(remiAddResults());
+    this.registrator = remi(this);
+    this.registrator.hook(remiRunner());
+    this.registrator.hook(remiAddResults());
     this.options = options;
   }
   getOptions(pluginSlug) {
-    let result = {port: 9222, url: "http://topcoat.io"};
-    if(this.options.hasOwnProperty(pluginSlug) && this.options.hasOwnProperty('default')){
+    let result = { port: 9222, url: 'http://topcoat.io' };
+    if (this.options.hasOwnProperty(pluginSlug) && this.options.hasOwnProperty('default')) {
       result = Object.assign(result, this.options.default, this.options[pluginSlug]);
-    } else if(this.options.hasOwnProperty(pluginSlug) && !this.options.hasOwnProperty('default')) {
+    } else if (this.options.hasOwnProperty(pluginSlug) && !this.options.hasOwnProperty('default')) {
       result = Object.assign(result, this.options[pluginSlug]);
-    } else if (this.options.hasOwnProperty('default')){
+    } else if (this.options.hasOwnProperty('default')) {
       result = Object.assign(result, this.options.default);
     }
     return result;
   }
   loadPlugins(thing) {
     return new Promise((resolve, reject) => {
-      if (typeof thing == 'string'){
-        let plugin = require(thing);
-        resolve([{register: plugin, options: this.getOptions(plugin.attributes.name)}]);
-      } else if (typeof thing == 'object' && Array.isArray(thing)) {
-        let plugins = thing.map((pluginSlug) => {
-          let plugin = require(pluginSlug);
-          return({register: plugin, options: this.getOptions(plugin.attributes.name)});
+      if (typeof thing === 'string') {
+        const plugin = require(thing);
+        resolve([{ register: plugin, options: this.getOptions(plugin.attributes.name) }]);
+      } else if (typeof thing === 'object' && Array.isArray(thing)) {
+        const plugins = thing.map((pluginSlug) => {
+          const plugin = require(pluginSlug);
+          return ({ register: plugin, options: this.getOptions(plugin.attributes.name) });
         });
         resolve(plugins);
       } else {
@@ -37,6 +37,6 @@ export default class Topmark {
     });
   }
   register(params) {
-    return this.loadPlugins(params).then((plugins) => this._registrator.register(plugins));
+    return this.loadPlugins(params).then((plugins) => this.registrator.register(plugins));
   }
 }
